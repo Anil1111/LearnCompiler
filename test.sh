c996df125da105252bcb2ac65835d2ea2e30d526
Munenori Oizumi <oizumi@be.to>
2018-11-17 09:36:56 -0800

    minicalc.c and its test is added.

--- test.sh
+++ test.sh
@@ -4,17 +4,27 @@ try_parser() {
     ./cp_tkn "$2" > tmp.txt
     diff --ignore-space-change tmp.txt "$1"
     if [ $? -ne 0 ]; then
-        echo "Output is different from expectation."
+        echo "cp_tkn: Output is different from expectation."
         exit 1
     fi
 }
 
 try_polish() {
-    yes "$2" | ./polish > tmp.txt
+    echo -e "$2" | ./polish > tmp.txt
     echo -e "$1" > target.txt
     diff --ignore-all-space --strip-trailing-cr tmp.txt target.txt
     if [ $? -ne 0 ]; then
-        echo "Output is different from expectation."
+        echo "polish: Output is different from expectation."
+        exit 1
+    fi
+}
+
+try_minicalc() {
+    echo -e "$2" | ./minicalc > tmp.txt
+    echo -e "$1" > expectation.txt
+    diff --ignore-all-space --strip-trailing-cr tmp.txt expectation.txt
+    if [ $? -ne 0 ]; then
+        echo "minicalc: Output is different from expectation."
         exit 1
     fi
 }
@@ -24,5 +34,7 @@ try_parser "token_parsed.txt" "token_txt.txt"
 try_polish "Input: Converted: ab+c+ \n Result: 6" a+b+c
 try_polish "Input: Converted: abc*+ \n Result: 7" a+b*c
 try_polish "Input: Converted: ab+5* \n Result: 15" "(a+b)*5"
+
+try_minicalc "610 \n 900 \n 5" "a=10\nb=20\nc=a+b*30\n?c\nc=(a+b)*30\n?c\n? 1+2+3+4-5\n"
 
 echo OK
